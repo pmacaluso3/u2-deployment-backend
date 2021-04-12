471cb1a69b7491fbe5259a515a3d69429494251b
pmacaluso3 <moxious.manoeuvre@gmail.com>
2021-04-12 11:53:59 -0400

    logging

--- models/index.js
+++ models/index.js
@@ -9,6 +9,12 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
+
+console.log('!!!!!');
+console.log(config);
+console.log(config.use_env_variable);
+console.log(process.env[config.use_env_variable]);
+
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
